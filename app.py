6ed821b4ec77e1a807f1f66b25cdc5764523e107
Luis Jose Mendez <81307629+mendez-luisjose@users.noreply.github.com>
2023-07-28 15:18:41 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,212 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from ultralytics import YOLO
+import cv2
+import easyocr
+import pandas as pd
+from util import set_background, write_csv
+import uuid
+import os
+
+set_background("./imgs/background.png")
+
+folder_path = "./licenses_plates_imgs_detected/"
+LICENSE_MODEL_DETECTION_DIR = './models/license_plate_detector.pt'
+COCO_MODEL_DIR = "./models/yolov8n.pt"
+
+reader = easyocr.Reader(['en'], gpu=False)
+
+vehicles = [2]
+
+header = st.container()
+body = st.container()
+
+coco_model = YOLO(COCO_MODEL_DIR)
+license_plate_detector = YOLO(LICENSE_MODEL_DETECTION_DIR)
+
+threshold = 0.15
+
+def read_license_plate(license_plate_crop, img):
+
+    scores = 0
+    detections = reader.readtext(license_plate_crop)
+
+    print(detections)
+    width = img.shape[1]
+    height = img.shape[0]
+    
+    if detections == [] :
+        return None, None
+
+    rectangle_size = license_plate_crop.shape[0]*license_plate_crop.shape[1]
+
+    plate = [] 
+
+    for result in detections:
+        length = np.sum(np.subtract(result[0][1], result[0][0]))
+        height = np.sum(np.subtract(result[0][2], result[0][1]))
+        
+        if length*height / rectangle_size > 0.20:
+            bbox, text, score = result
+            text = result[1]
+            text = text.upper()
+            scores += score
+            plate.append(text)
+            
+    return " ".join(plate), scores/len(plate)
+
+def model_prediction(img):
+    license_numbers = 0
+    results = {}
+    licenses_texts = []
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    object_detections = coco_model(img)[0]
+    license_detections = license_plate_detector(img)[0]
+
+    if len(object_detections.boxes.cls.tolist()) != 0 :
+        for detection in object_detections.boxes.data.tolist() :
+            xcar1, ycar1, xcar2, ycar2, car_score, class_id = detection
+
+            if int(class_id) in vehicles :
+                #detections_.append([xcar1, ycar1, xcar2, ycar2, car_score])
+                cv2.rectangle(img, (int(xcar1), int(ycar1)), (int(xcar2), int(ycar2)), (0, 0, 255), 3)
+    else :
+            xcar1, ycar1, xcar2, ycar2 = 0, 0, 0, 0
+            car_score = 0
+
+    if len(license_detections.boxes.cls.tolist()) != 0 :
+        license_plate_crops_total = []
+        for license_plate in license_detections.boxes.data.tolist() :
+            x1, y1, x2, y2, score, class_id = license_plate
+
+            cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)
+
+            license_plate_crop = img[int(y1):int(y2), int(x1): int(x2), :]
+
+            img_name = '{}.jpg'.format(uuid.uuid1())
+        
+            cv2.imwrite(os.path.join(folder_path, img_name), license_plate_crop)
+
+            license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY) 
+
+            license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_gray, img)
+
+            licenses_texts.append(license_plate_text)
+
+            if license_plate_text is not None and license_plate_text_score is not None  :
+                license_plate_crops_total.append(license_plate_crop)
+                results[license_numbers] = {}
+                
+                results[license_numbers][license_numbers] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2], 'car_score': car_score},
+                                                        'license_plate': {'bbox': [x1, y1, x2, y2],
+                                                                            'text': license_plate_text,
+                                                                            'bbox_score': score,
+                                                                            'text_score': license_plate_text_score}} 
+                license_numbers+=1
+          
+        write_csv(results, f"./csv_detections/detection_results.csv")
+
+        img_wth_box = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+        return [img_wth_box, licenses_texts, license_plate_crops_total]
+    
+    else : 
+        img_wth_box = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return [img_wth_box]
+    
+with header :
+    _, col1, _ = st.columns([0.2,1,0.1])
+    col1.title("💥 License Car Plate Detection 🚗")
+
+    _, col0, _ = st.columns([0.15,1,0.1])
+    col0.image("./imgs/test_background.jpg", width=500)
+
+
+    _, col4, _ = st.columns([0.1,1,0.2])
+    col4.subheader("Computer Vision Detection with YoloV8 🧪")
+
+    _, col, _ = st.columns([0.3,1,0.1])
+    col.image("./imgs/plate_test.jpg")
+
+    _, col5, _ = st.columns([0.05,1,0.1])
+
+    st.write("The differents models detect the car and the license plate in a given image, then extracts the info about the license using EasyOCR, and crop and save the license plate as a Image, with a CSV file with all the data.   ")
+
+
+with body :
+    _, col1, _ = st.columns([0.1,1,0.2])
+    col1.subheader("Check It-out the License Car Plate Detection Model 🔎!")
+
+    img = st.file_uploader("Upload a Car Image: ", type=["png", "jpg", "jpeg"])
+
+    _, col2, _ = st.columns([0.3,1,0.2])
+
+    _, col5, _ = st.columns([0.8,1,0.2])
+
+    
+    if img is not None:
+        image = np.array(Image.open(img))    
+        col2.image(image, width=400)
+
+        if col5.button("Apply Detection"):
+            results = model_prediction(image)
+
+            if len(results) == 3 :
+                prediction, texts, license_plate_crop = results[0], results[1], results[2]
+                print(results[1], len(license_plate_crop))
+
+                texts = [i for i in texts if i is not None]
+                
+                if len(texts) == 1 and len(license_plate_crop) :
+                    _, col3, _ = st.columns([0.4,1,0.2])
+                    col3.header("Detection Results ✅:")
+
+                    _, col4, _ = st.columns([0.1,1,0.1])
+                    col4.image(prediction)
+
+                    _, col9, _ = st.columns([0.4,1,0.2])
+                    col9.header("License Cropped ✅:")
+
+                    _, col10, _ = st.columns([0.3,1,0.1])
+                    col10.image(license_plate_crop[0], width=350)
+
+                    _, col11, _ = st.columns([0.45,1,0.55])
+                    col11.success(f"License Number: {texts[0]}")
+
+                    df = pd.read_csv(f"./csv_detections/detection_results.csv")
+                    st.dataframe(df)
+                elif len(texts) > 1 and len(license_plate_crop) > 1  :
+                    _, col3, _ = st.columns([0.4,1,0.2])
+                    col3.header("Detection Results ✅:")
+
+                    _, col4, _ = st.columns([0.1,1,0.1])
+                    col4.image(prediction)
+
+                    _, col9, _ = st.columns([0.4,1,0.2])
+                    col9.header("License Cropped ✅:")
+
+                    _, col10, _ = st.columns([0.3,1,0.1])
+
+                    _, col11, _ = st.columns([0.45,1,0.55])
+
+                    col7, col8 = st.columns([1,1])
+                    for i in range(0, len(license_plate_crop)) :
+                        col10.image(license_plate_crop[i], width=350)
+                        col11.success(f"License Number {i}: {texts[i]}")
+
+                    df = pd.read_csv(f"./csv_detections/detection_results.csv")
+                    st.dataframe(df)
+            else :
+                prediction = results[0]
+                _, col3, _ = st.columns([0.4,1,0.2])
+                col3.header("Detection Results ✅:")
+
+                _, col4, _ = st.columns([0.3,1,0.1])
+                col4.image(prediction)
+
+
+
+
+ 
---
+++ util.py
@@ -0,0 +1,114 @@
+import easyocr
+import base64
+import streamlit as st
+
+def set_background(image_file):
+    """
+    This function sets the background of a Streamlit app to an image specified by the given image file.
+
+    Parameters:
+        image_file (str): The path to the image file to be used as the background.
+
+    Returns:
+        None
+    """
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+
+# Initialize the OCR reader
+reader = easyocr.Reader(['en'], gpu=False)
+
+# Mapping dictionaries for character conversion
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S'}
+
+
+def write_csv(results, output_path):
+    """
+    Write the results to a CSV file.
+
+    Args:
+        results (dict): Dictionary containing the results.
+        output_path (str): Path to the output CSV file.
+    """
+    with open(output_path, 'w') as f:
+        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
+                                                'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
+                                                'license_number_score'))
+
+        for frame_nmr in results.keys():
+            for car_id in results[frame_nmr].keys():
+                print(results[frame_nmr][car_id])
+                if 'car' in results[frame_nmr][car_id].keys() and \
+                   'license_plate' in results[frame_nmr][car_id].keys() and \
+                   'text' in results[frame_nmr][car_id]['license_plate'].keys():
+                    f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
+                                                            car_id,
+                                                            '[{} {} {} {}]'.format(
+                                                                results[frame_nmr][car_id]['car']['bbox'][0],
+                                                                results[frame_nmr][car_id]['car']['bbox'][1],
+                                                                results[frame_nmr][car_id]['car']['bbox'][2],
+                                                                results[frame_nmr][car_id]['car']['bbox'][3]),
+                                                            '[{} {} {} {}]'.format(
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][0],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][1],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][2],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][3]),
+                                                            results[frame_nmr][car_id]['license_plate']['bbox_score'],
+                                                            results[frame_nmr][car_id]['license_plate']['text'],
+                                                            results[frame_nmr][car_id]['license_plate']['text_score'])
+                            )
+        f.close()
+
+def read_license_plate(license_plate_crop):
+    """
+    Read the license plate text from the given cropped image.
+
+    Args:
+        license_plate_crop (PIL.Image.Image): Cropped image containing the license plate.
+
+    Returns:
+        tuple: Tuple containing the formatted license plate text and its confidence score.
+    """
+
+    detections = reader.readtext(license_plate_crop)
+    print(detections)
+
+    if detections == [] :
+        return None, None
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        #text = text.upper().replace(' ', '')
+        text = text.upper()
+        print(text)
+
+        if text is not None and score is not None and bbox is not None and len(text) >= 6:
+        #if license_complies_format(text):
+        #    return format_license(text), score
+            return text, score
+
+    return None, None
+
